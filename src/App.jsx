380e8365da093a2d6f636af9b602f59b60a8d5e2
Josh Mann <jlmann72@gmail.com>
2020-09-29 19:51:44 -0400

    Calculator mockup with html and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function handleOperandClick(props) {}
 
 function App() {
-  return <div>Hello, World!</div>
+  const [result, setResult] = useState(0)
+  const [operands, setOperands] = useState([])
+  const [operators, setOperators] = useState([])
+
+  return (
+    <div>
+      <ul className="calculatorDisplay">
+        <li className="readout">{result}</li>
+        <li className="clear">AC</li>
+        <li className="operatorButton" operatorValue="/">
+          /
+        </li>
+        <li
+          className="digitButton"
+          onClick="handleOperandClick"
+          operandValue="7"
+        >
+          7
+        </li>
+        <li className="digitButton">8</li>
+        <li className="digitButton">9</li>
+        <li className="operatorButton">*</li>
+        <li className="digitButton">4</li>
+        <li className="digitButton">5</li>
+        <li className="digitButton">6</li>
+        <li className="operatorButton">-</li>
+        <li className="digitButton">1</li>
+        <li className="digitButton">2</li>
+        <li className="digitButton">3</li>
+        <li className="operatorButton">+</li>
+        <li className="zeroButton">0</li>
+        <li className="digitButton">.</li>
+        <li className="operatorButton">=</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App
