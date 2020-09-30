76609cb837d96609ed2c4d39f074a7de515f74e3
Josh Mann <jlmann72@gmail.com>
2020-09-30 16:39:12 -0400

    Clicking operator buttons calls function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
 
 export function Calculator() {
-  const [display, setDisplay] = useState(42)
+  // States
+  const [display, setDisplay] = useState(0)
+  const [firstOperand, setFirstOperand] = useState(null)
+  const [operator, setOperator] = useState(null)
 
+  // event handler functions
   function clickClearButton(event) {
     setDisplay(0)
+  }
+
+  function clickDigit(digit) {
+    setFirstOperand(digit)
+  }
+
+  function clickOperator(chosenOperator) {
+    setOperator(chosenOperator)
+    console.log(chosenOperator)
   }
 
   return (
@@ -17,20 +30,68 @@ export function Calculator() {
           </button>
           <button className="button fn">&#177;</button>
           <button className="button fn">&#37;</button>
-          <button className="button op">&#247;</button>
-          <button className="button">7</button>
-          <button className="button">8</button>
-          <button className="button">9</button>
-          <button className="button op">&#215;</button>
-          <button className="button">4</button>
-          <button className="button">5</button>
-          <button className="button">6</button>
-          <button className="button op">&#8722;</button>
-          <button className="button">1</button>
-          <button className="button">2</button>
-          <button className="button">3</button>
-          <button className="button op">&#43;</button>
-          <button className="button x2">0</button>
+          <button
+            className="button op"
+            onClick={() => {
+              clickOperator('/')
+            }}
+          >
+            &#247;
+          </button>
+          <button className="button" onClick={() => clickDigit(7)}>
+            7
+          </button>
+          <button className="button" onClick={() => clickDigit(8)}>
+            8
+          </button>
+          <button className="button" onClick={() => clickDigit(9)}>
+            9
+          </button>
+          <button
+            className="button op"
+            onClick={() => {
+              clickOperator('*')
+            }}
+          >
+            &#215;
+          </button>
+          <button className="button" onClick={() => clickDigit(4)}>
+            4
+          </button>
+          <button className="button" onClick={() => clickDigit(5)}>
+            5
+          </button>
+          <button className="button" onClick={() => clickDigit(6)}>
+            6
+          </button>
+          <button
+            className="button op"
+            onClick={() => {
+              clickOperator('-')
+            }}
+          >
+            &#8722;
+          </button>
+          <button className="button" onClick={() => clickDigit(1)}>
+            1
+          </button>
+          <button className="button" onClick={() => clickDigit(2)}>
+            2
+          </button>
+          <button className="button" onClick={() => clickDigit(3)}>
+            3
+          </button>
+          <button
+            className="button op"
+            onClick={() => {
+              clickOperator('+')
+            }}
+          >
+            &#43;
+          </button>
+          <button className="button x2" onClick={() => clickDigit(0)}>
+            0
+          </button>
           <button className="button">.</button>
           <button className="button op">&#61;</button>
         </div>
